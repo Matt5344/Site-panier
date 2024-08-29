095136ccbb4d29f52ba405a10ae7b374fae007f4
Matt5344 <rasclematteoga@gmail.com>
2024-08-29 14:47:27 +0200

    Mise en ligne

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boutique</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="panier.php" class="link">Panier <span>8</span></a>
+    <section class="products_list">
+        <form action="" class="product">
+            <div class="image_product">
+                <img src="images/img1.png">
+            </div>
+            <div class="content">
+                <h4 class="name">Meat Frite</h4>
+                <h2 class="price">12€</h2>
+                <a href="#" class="id_product">Ajouter au panier</a>
+            </div>
+        </form>
+        
+
+    </section>
+</body>
+</html>
---
+++ panier.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panier</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="panier">
+    <a href="index.php" class="link">Boutique</a>
+    <section>
+        <table>
+            <tr>
+                <th></th>
+                <th>Nom</th>
+                <th>Prix</th>
+                <th>Quantité</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td><img src="images/img1.png"></td>
+                <td>Meat</td>
+                <td>12€</td>
+                <td>5</td>
+                <td><img src="images/delete.png"></td>
+            </tr>
+            
+
+            <tr class="total">
+                <th>Total : 25€</th>
+            </tr>
+        </table>
+    </section>
+</body>
+</html>
